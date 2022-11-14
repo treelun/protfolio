4e2168fb70679ca59c1c3fa6ac126c2f86ff9938
treelun <109446729+treelun@users.noreply.github.com>
2022-11-14 17:29:02 +0900

    portfolio-01

---
+++ Avcorad/Assets/Resource/Low Poly Knights/Scripts/cameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public GameObject lookAtObject;
+    public float smoothness = 1;
+    Vector3 zPosition;
+     
+    void Start() {
+        zPosition = transform.position -lookAtObject.transform.position;
+    }
+     
+    void LateUpdate() {
+		Vector3 detination = lookAtObject.transform.position + zPosition;
+		Vector3 pos = Vector3.Lerp(transform.position, detination, Time.deltaTime * smoothness);
+        
+		transform.position = pos;
+        transform.LookAt(lookAtObject.transform);
+	}
+}
