0533fafbe08914e63527861e4c7e99ed2a43c70e
treelun <109446729+treelun@users.noreply.github.com>
2022-11-15 16:34:04 +0900

    portfolio-03

--- Avcorad/Assets/Script/NewScript/Weapon.cs
+++ Avcorad/Assets/Script/NewScript/Weapon.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour,Iitem
+public class Weapon : MonoBehaviour, Iitem
 {
-    public enum WeaponType { basicWeapon, SecondWeapon}
+    public enum WeaponType { basicWeapon, SecondWeapon }
     public WeaponType weaponType;
 
     public float AttackForce { get; set; }
     public float AttackSpeed { get; set; }
     public ParticleSystem hitparticle;
     public ParticleSystem swingparticle;
+
     public void useItem()
     {
         //플레이어의 무기데이터 업데이트
